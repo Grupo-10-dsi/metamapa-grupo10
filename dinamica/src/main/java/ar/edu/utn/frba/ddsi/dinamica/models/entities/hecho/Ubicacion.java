b4ba5926939d9528d4162eb7fa3ab4fa4627e905
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2025-06-10 19:39:24 -0300

    Pruebo commit con colaborador
    Co-authored-by: Nicolas Gabriel  <Ranico1@users.noreply.github.com>

--- dinamica/src/main/java/ar/edu/utn/frba/ddsi/dinamica/models/entities/hecho/Ubicacion.java
+++ dinamica/src/main/java/ar/edu/utn/frba/ddsi/dinamica/models/entities/hecho/Ubicacion.java
@@ -1,5 +1,10 @@
 package ar.edu.utn.frba.ddsi.dinamica.models.entities.hecho;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Ubicacion {
     private Double latitud;
     private Double longitud;
@@ -8,4 +13,6 @@ public class Ubicacion {
         this.latitud = latitud;
         this.longitud = longitud;
     }
+
+    public Ubicacion() {}
 }
