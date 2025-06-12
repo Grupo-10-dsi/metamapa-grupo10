fd1697ef232eb382b6a250ef0241913fea931f9e
Nicolas <naguero@frba.utn.edu.ar>
2025-06-12 17:23:53 -0300

    subo test de fuente estatica.

--- estatica/src/test/java/ar/edu/utn/frba/ddsi/estatica/EstaticaApplicationTests.java
+++ estatica/src/test/java/ar/edu/utn/frba/ddsi/estatica/EstaticaApplicationTests.java
@@ -28,7 +28,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-//fg
+//fgf
 @SuppressWarnings("removal")
 @SpringBootTest
 @AutoConfigureMockMvc
