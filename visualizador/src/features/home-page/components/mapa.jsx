2cf6ab569599594a751c1948e661c8fc3790bd4f
provoleta <buhlertomas@gmail.com>
2025-10-25 13:20:49 -0300

    Cambio el mapa para usar leaflet

--- visualizador/src/features/home-page/components/mapa.jsx
+++ visualizador/src/features/home-page/components/mapa.jsx
@@ -1,23 +1,29 @@
 // visualizador/src/features/home-page/components/mapa.jsx
 import {Card} from "react-bootstrap";
 import React from "react";
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import './mapa.css'
 
+
+// -34.37049232747865, -58.90407374255551
 //TODO: Reemplazar el mapa de google con uno que permita agregar marcadores
 const Mapa = () => (
     <Card className="shadow-lg" style={{ marginTop: '50px', marginBottom: '50px' }}>
         <Card.Body style={{ padding: '0px' }}>
             <h4 className="text-center pt-3 pb-3 mb-0">Mapa de hechos registrados</h4>
             <div style={{ height: '450px', width: '100%' }}>
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d210146.6827477399!2d-58.5733847849977!3d-34.61574369999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3b4ef90cbd%3A0xa0b3812e88e88e87!2sBuenos%20Aires%2C%20CABA!5e0!3m2!1ses-419!2sar!4v1685126459021!5m2!1ses-419!2sar"
-                    width="100%"
-                    height="100%"
-                    style={{ border: 0 }}
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                    title="Google Maps Embed"
-                ></iframe>
+                <MapContainer
+                    center={{lat: '-34.37049232747865', lng: '-58.90407374255551'}}
+                    zoom={10} >
+                    <TileLayer
+                        url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    <Marker position={{lat: '-34.37049232747865', lng: '-58.90407374255551'}}> {/* icono no carga, ver esto */}
+                        <Popup>Aca van los hechos</Popup>
+                    </Marker>
+                </MapContainer>
             </div>
         </Card.Body>
     </Card>
