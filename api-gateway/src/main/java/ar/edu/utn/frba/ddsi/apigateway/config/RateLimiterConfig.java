8dfc9a2f28178f0d018434e588eafe8a5609a040
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2025-12-13 22:34:09 -0300

    Configuracion del ratelimiter

--- api-gateway/src/main/java/ar/edu/utn/frba/ddsi/apigateway/config/RateLimiterConfig.java
+++ api-gateway/src/main/java/ar/edu/utn/frba/ddsi/apigateway/config/RateLimiterConfig.java
@@ -4,7 +4,10 @@ import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
+
+import java.net.InetSocketAddress;
 import java.util.Objects;
+import java.util.Optional;
 
 @Configuration
 public class RateLimiterConfig {
@@ -13,10 +16,16 @@ public class RateLimiterConfig {
     public KeyResolver ipKeyResolver() {
         // Resuelve la IP del cliente para usarla como clave del límite
         return exchange -> Mono.just(
-                Objects.requireNonNull(exchange.getRequest().getRemoteAddress())
-                        .getAddress().getHostAddress()
+                Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("X-Forwarded-For"))
+                        .map(ip -> ip.split(",")[0].trim()) // Si hay multiples proxies, toma la IP original
+                        .orElseGet(() -> Optional.ofNullable(exchange.getRequest().getRemoteAddress())
+                                .map(InetSocketAddress::getAddress)
+                                .map(addr -> addr.getHostAddress())
+                                .orElse("unknown") // Fallback seguro si no se puede determinar la IP
+                        )
         );
     }
+
 
     // OPCIONAL: Limitar por usuario logueado (JWT)
     // @Bean
