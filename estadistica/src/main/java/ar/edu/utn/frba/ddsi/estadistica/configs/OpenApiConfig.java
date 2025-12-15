adf057e698d24dd9895852c578807110e2c4b045
zBlooDy <tperezgiacchetta@frba.utn.edu.ar>
2025-12-15 00:06:59 -0300

    Pruebo agregar el swagger

--- estadistica/src/main/java/ar/edu/utn/frba/ddsi/estadistica/configs/OpenApiConfig.java
+++ estadistica/src/main/java/ar/edu/utn/frba/ddsi/estadistica/configs/OpenApiConfig.java
@@ -2,17 +2,25 @@ package ar.edu.utn.frba.ddsi.estadistica.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://metamapa.page/swagger-estadistica");
+        server.setDescription("Servidor de Producción");
+
         return new OpenAPI()
+                .servers(List.of(server))
                 .info(new Info()
-                        .title("API de Estadísticas")
+                        .title("API de Estadísticas - MetaMapa")
                         .version("1.0")
                         .description("Documentación de endpoints del módulo de estadísticas"));
     }
