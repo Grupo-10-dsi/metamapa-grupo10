e76f206a8675cb6edc8868a80777d28d432cf89c
LucasArias <lucaarias@frba.utn.edu.ar>
2025-12-15 14:04:20 -0300

    fix en el GraphQLTypeResolverConfig, usaba los DTO viejos y petaba pq no encontraba instancias

--- agregador/src/main/java/ar/edu/utn/frba/ddsi/agregador/config/GraphQLTypeResolverConfig.java
+++ agregador/src/main/java/ar/edu/utn/frba/ddsi/agregador/config/GraphQLTypeResolverConfig.java
@@ -1,7 +1,7 @@
 package ar.edu.utn.frba.ddsi.agregador.config;
 
-import ar.edu.utn.frba.ddsi.agregador.models.entities.dtos.HechoMultimediaDTO;
-import ar.edu.utn.frba.ddsi.agregador.models.entities.dtos.HechoTextualDTO;
+import ar.edu.utn.frba.ddsi.agregador.models.entities.dtos.HechoMultimediaDTOGraph;
+import ar.edu.utn.frba.ddsi.agregador.models.entities.dtos.HechoTextualDTOGraph;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
@@ -14,10 +14,10 @@ public class GraphQLTypeResolverConfig {
         return wiringBuilder -> wiringBuilder
                 .type("Hecho", typeWiring -> typeWiring.typeResolver(env -> {
                     Object src = env.getObject();
-                    if (src instanceof HechoTextualDTO) {
+                    if (src instanceof HechoTextualDTOGraph) {
                         return env.getSchema().getObjectType("HechoTextual");
                     }
-                    if (src instanceof HechoMultimediaDTO) {
+                    if (src instanceof HechoMultimediaDTOGraph) {
                         return env.getSchema().getObjectType("HechoMultimedia");
                     }
                     return null;
